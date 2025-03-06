41daeab0e9219c3eb1f82558ac02d3dd51bf45f8
Kim <Michael2005H@outlook.com>
2025-03-06 19:27:13 +0800

    系统菜单

--- src/view/admin/system/system.vue
+++ src/view/admin/system/system.vue
@@ -1,6 +1,18 @@
 <script setup lang="ts"></script>
 <template>
-    <div>系统</div>
+    <div class="system_view">
+      <div class="system_menu">11</div>
+    </div>
 </template>
 <style lang="scss">
+.system_menu {
+  display: flex;
+  height: 100vh;
+  .system_menu{
+    width: 160px;
+    height: calc(100% - 90px);
+    border: 10px solid red;
+    background-color: var(--color-bg-1);
+  }
+}
 </style>
